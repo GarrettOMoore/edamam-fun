7c50d22a82a661ae7f390e06d601dbe5ced1611a
Garrett Moore <robertgarrettmoore@gmail.com>
2019-06-09 22:36:43 -0700

    displaying more recipe data

--- client/src/Components/Recipes.jsx
+++ client/src/Components/Recipes.jsx
@@ -14,19 +14,30 @@ class Recipes extends Component {
     componentDidMount(){
         axios.get('/recipes').then((res) => {
             this.setState({
-                data: res.data
+                data: res.data.data.hits
             })
         })
         .catch((err) => console.log(err))
     }
 
     render() {
+        let allRecipes = Array.from(this.state.data);
+        let recipes = allRecipes.map((recipe, index) => {
+            return(
+                <>
+                <p>{recipe.recipe.label}</p>
+                <img className='pantry-pic'width={'20%'} height={'10%'}src={recipe.recipe.image} alt={recipe.recipe.label}/><br/>
+                <a href={recipe.recipe.url}>Link</a>
+                </>
+            )
+        })
         return(
             <div className='diet-box'>
             <p>RECIPE PAGE...</p>
             {/* <form>
                 <input type="radio" name="vegetarian" value="vegetarian"/><p>Vegetarian</p>
             </form> */}
+            {recipes}
             </div>
         )
     }
--- routes/recipes.js
+++ routes/recipes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const axios = require('axios');
 
 router.get('/', (req,res) => {
-	axios.get(`https://api.edamam.com/search?q=salmon&app_id=${process.env.RECIPE_ID}&app_key=${process.env.RECIPE_KEY}`, {
+	axios.get(`https://api.edamam.com/search?q=shallot?salmon?avocado&app_id=${process.env.RECIPE_ID}&app_key=${process.env.RECIPE_KEY}`, {
   }).then((result)=> {
 		console.log(result.data)
       res.json({data: result.data})
